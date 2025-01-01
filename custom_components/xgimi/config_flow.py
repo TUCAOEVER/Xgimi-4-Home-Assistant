c4a50b3c30e5034922f9cb2fa6b84163e6a166f4
TUCAOEVER <43756134+TUCAOEVER@users.noreply.github.com>
2025-01-02 00:32:29 +0800

    更新
    - 端口配置项更新

--- custom_components/xgimi/config_flow.py
+++ custom_components/xgimi/config_flow.py
@@ -1,6 +1,9 @@
+"""XGIMIConfig flow."""
+
 from __future__ import annotations
 
 from typing import Any
+
 import voluptuous as vol
 
 from homeassistant import config_entries
@@ -8,31 +11,45 @@ from homeassistant.const import CONF_HOST, CONF_NAME, CONF_TOKEN
 from homeassistant.data_entry_flow import FlowResult
 from homeassistant.util.network import is_host_valid
 
-from .const import (
-    DOMAIN,
-    CONF_COMMAND_PORT,
-    CONF_ADVANCE_PORT,
-    CONF_RESPONSE_PORT,
-)
+from .const import DOMAIN
+from .entity import XGIMIEntity
+from .xgimi import XGIMIBaseEntity
 
 
-class XgimiConfigFLow(config_entries.ConfigFlow, domain=DOMAIN):
+class XGIMIConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """ConfigFlow 实现类."""
+
     VERSION = 1
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
     ) -> FlowResult:
+        """初始化信息以及用户界面."""
         errors: dict[str, str] = {}
 
         if user_input is not None:
             host = user_input[CONF_HOST]
             name = user_input[CONF_NAME]
             token = user_input[CONF_TOKEN]
-            command_port = user_input[CONF_COMMAND_PORT]
-            advance_port = user_input[CONF_ADVANCE_PORT]
-            response_port = user_input[CONF_RESPONSE_PORT]
+            pinging_port = user_input.get("pinging_port")
+            command_port = user_input.get("command_port")
+            advance_port = user_input.get("advance_port")
+            xgimi_entity = XGIMIEntity(
+                name=name,
+                unique_id=f"{name}-{token}",
+                base_entity=XGIMIBaseEntity(
+                    ip=host,
+                    command_port=command_port,
+                    advance_port=advance_port,
+                    pinging_port=pinging_port,
+                    manufacturer_data=token,
+                    is_on=False,
+                ),
+            )
             if not is_host_valid(host):
                 errors[CONF_HOST] = "invalid_host"
+            elif not await xgimi_entity.async_check_alive():
+                errors[CONF_HOST] = "cannot_connect"
             else:
                 await self.async_set_unique_id(f"{name}-{token}")
                 self._abort_if_unique_id_configured()
@@ -55,9 +72,15 @@ class XgimiConfigFLow(config_entries.ConfigFlow, domain=DOMAIN):
                     vol.Required(
                         CONF_TOKEN, default=user_input.get(CONF_TOKEN, vol.UNDEFINED)
                     ): str,
-                    vol.Optional(CONF_COMMAND_PORT, default=16735): int,
-                    vol.Optional(CONF_ADVANCE_PORT, default=16750): int,
-                    vol.Optional(CONF_RESPONSE_PORT, default=554): int,
+                    vol.Required(
+                        "pinging_port", default=user_input.get("pinging_port", 554)
+                    ): int,
+                    vol.Optional(
+                        "command_port", default=user_input.get("command_port", 16735)
+                    ): int,
+                    vol.Optional(
+                        "advance_port", default=user_input.get("advance_port", 16750)
+                    ): int,
                 }
             ),
             errors=errors,
